58aa3a4a86f98021ad0d03e79c3988441f540ec5
Jhol Moreira Mendanha <galilleu2011@gmail.com>
2022-05-04 21:19:36 -0300

    criação de banco com ORM

---
+++ src/main/java/com/dio/santander/banklinesantanderapi/BanklineSantanderApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banklinesantanderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineSantanderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanklineSantanderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dio/santander/banklinesantanderapi/BanklineSantanderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banklinesantanderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineSantanderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
